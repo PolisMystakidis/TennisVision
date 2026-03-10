f074f054dde729d3239e9bc8028a4c503f606ddf
Polis Mystakidis <polis.mistak@gmail.com>
2026-03-10 13:05:45 +0200

    Detect line for first 5 frames then take avrg

--- line_detection.py
+++ line_detection.py
@@ -3,29 +3,38 @@ import numpy as np
 import glob
 import torch
 import os
-from TennisCourtDetector.tracknet import BallTrackerNet
+import torch.nn as nn
+import torch
 import torch.nn.functional as F
-from TennisCourtDetector.postprocess import postprocess, refine_kps
-from TennisCourtDetector.homography import get_trans_matrix, refer_kps
+from post_process import postprocess, refine_kps , postprocess_ball
+from homography import get_trans_matrix, refer_kps
+from models import CourtTrackerNet , BallTrackerNet
+from tqdm import tqdm
+from scipy.spatial import distance
+
 OUTPUT_WIDTH = 640
 OUTPUT_HEIGHT = 360
-model_dir = "D:\\jupyter_server\\TennisVision\\model_tennis_court_det.pt"
+court_model_dir = "D:\\jupyter_server\\TennisVision\\model_tennis_court_det.pt"
+ball_model_dir = "D:\\jupyter_server\\TennisVision\\model_ball.pt"
 image_dir = "D:\\jupyter_server\\datasets\\data\\images"
 image_id = "-_5ljBK4HnI_200.png"
 out_path = "D:\\jupyter_server\\TennisVision\\outputs"
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print("Device : ",device)
-def detect_lines(image,use_refine_kps=True,use_homography=True):
+
+model = CourtTrackerNet(out_channels=15)
+model.load_state_dict(torch.load(court_model_dir, map_location=device))
+model = model.to(device)
+
+ball_model = BallTrackerNet()
+ball_model.load_state_dict(torch.load(ball_model_dir, map_location=device))
+ball_model = ball_model.to(device)
+
+def detect_lines(image,model,use_refine_kps=True,use_homography=True):
     inp = (image.astype(np.float32) / 255.)
     inp = torch.tensor(np.rollaxis(inp, 2, 0))
     inp = inp.unsqueeze(0)
-
-    model = BallTrackerNet(out_channels=15)
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    model = model.to(device)
-    model.load_state_dict(torch.load(model_dir, map_location=device))
     model.eval()
-
     out = model(inp.float().to(device))[0]
     pred = F.sigmoid(out).detach().cpu().numpy()
     points = []
@@ -40,7 +49,7 @@ def detect_lines(image,use_refine_kps=True,use_homography=True):
         matrix_trans = get_trans_matrix(points)
         if matrix_trans is not None:
             points = cv2.perspectiveTransform(refer_kps, matrix_trans)
-            points = [np.squeeze(x) for x in points]
+            points = [np.squeeze(x).tolist() for x in points]
     return points
 def get_intersection(line1, line2):
     """Calculates the intersection of two lines given by (x1, y1, x2, y2)"""
@@ -55,6 +64,57 @@ def get_intersection(line1, line2):
     py = ((x1*y2 - y1*x2)*(y3 - y4) - (y1 - y2)*(x3*y4 - y3*x4)) / denom
     return int(px), int(py)
 
+def infer_model(frames, model,scaleH = None ,scaleW = None):
+    """ Run pretrained model on a consecutive list of frames    
+    :params
+        frames: list of consecutive video frames
+        model: pretrained model
+    :return    
+        ball_track: list of detected ball points
+        dists: list of euclidean distances between two neighbouring ball points
+    """
+    height = 360
+    width = 640
+    dists = [-1]*2
+    ball_track = [(None,None)]*2
+    for num in range(2, len(frames)):
+        img = cv2.resize(frames[num], (width, height))
+        img_prev = cv2.resize(frames[num-1], (width, height))
+        img_preprev = cv2.resize(frames[num-2], (width, height))
+        imgs = np.concatenate((img, img_prev, img_preprev), axis=2)
+        imgs = imgs.astype(np.float32)/255.0
+        imgs = np.rollaxis(imgs, 2, 0)
+        inp = np.expand_dims(imgs, axis=0)
+        out = model(torch.from_numpy(inp).float().to(device))
+        output = out.argmax(dim=1).detach().cpu().numpy()
+        x_pred, y_pred = postprocess_ball(output)
+        ball_track.append((x_pred, y_pred))
+        if ball_track[-1][0] and ball_track[-2][0]:
+            dist = distance.euclidean(ball_track[-1], ball_track[-2])
+        else:
+            dist = -1
+        dists.append(dist)
+    return ball_track, dists 
+
+def remove_outliers(ball_track, dists, max_dist = 100):
+    """ Remove outliers from model prediction    
+    :params
+        ball_track: list of detected ball points
+        dists: list of euclidean distances between two neighbouring ball points
+        max_dist: maximum distance between two neighbouring ball points
+    :return
+        ball_track: list of ball points
+    """
+    outliers = list(np.where(np.array(dists) > max_dist)[0])
+    for i in outliers:
+        if i+1 < len(dists) and i-1 >= 0 :
+            if (dists[i+1] > max_dist) | (dists[i+1] == -1):       
+                ball_track[i] = (None, None)
+                outliers.remove(i)
+            elif dists[i-1] == -1:
+                ball_track[i-1] = (None, None)
+    return ball_track  
+
 # 1. Setup the path to your folder
 video_folder = 'videos'
 # This grabs all mp4, avi, and mov files
@@ -66,21 +126,47 @@ for ext in video_extensions:
 for video_path in video_files:
     gif_path = f'{video_path}'
     cap = cv2.VideoCapture(gif_path)
-    end_points = []
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    last_3_frames = []
+    frame_id = 0
+    avrg_court_points = []
     if not cap.isOpened():
         print("Error: Could not open GIF.")
         exit()
-
     while cap.isOpened():
         ret, frame = cap.read()
         frame = cv2.resize(frame, (OUTPUT_WIDTH, OUTPUT_HEIGHT))
         if not ret: break
-        points = detect_lines(frame)
-        for j in range(len(points)):
-            if points[j][0] is not None:
-                image = cv2.circle(frame, (int(points[j][0]), int(points[j][1])),
-                                radius=0, color=(0, 0, 255), thickness=10)
+        else:
+            last_3_frames.append(frame)
+            last_3_frames = last_3_frames[-3::]
+        #Ball Detection
+        ball_track , dist = infer_model(last_3_frames,ball_model)
+        ball_track = remove_outliers(ball_track, dist)
+
+        # Court Detection
+        # Detect line for first 5 frames then take avrg 
+        if len(avrg_court_points) < 5:
+            court_points = detect_lines(frame,model,use_refine_kps=True,use_homography=True)
+            if (None,None) not in court_points:
+                avrg_court_points.append(court_points)
+        elif len(avrg_court_points) >= 5:
+            court_points = np.asarray(avrg_court_points).sum(axis=0)/5
+            court_points = court_points.tolist()
+
+        # Draw on image
+        image = frame
+        if ball_track[-1][0] is not None and ball_track[-1][1] is not None:  
+            x_ball_pred , y_ball_pred = ball_track[-1]
+            image = cv2.circle(image, (int(x_ball_pred), int(y_ball_pred)),
+                                    radius=0, color=(255,0, 0), thickness=10)
+        for j in range(len(court_points)):
+            if court_points[j][0] is not None:
+                image = cv2.circle(image, (int(court_points[j][0]), int(court_points[j][1])),
+                            radius=0, color=(0,255, 0), thickness=10)
+
         cv2.imshow('Court Intersection Corners',image)
+        frame_id += 1
         if cv2.waitKey(30) & 0xFF == ord('q'):
             break
     cap.release()
