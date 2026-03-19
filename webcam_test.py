1930121fb942fede9c86b5302d60f3f36f4ded86
Polis Mystakidis <polis.mistak@gmail.com>
2026-03-19 14:16:20 +0200

    Managing HVS image to keep only ball colours

--- webcam_test.py
+++ webcam_test.py
@@ -2,6 +2,31 @@ import cv2
 import numpy as np
 from line_detection import infer_model,remove_outliers,ball_model,OUTPUT_HEIGHT,OUTPUT_WIDTH
 
+prevCicle = None
+dist = lambda x1,y1,x2,y2: (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2)
+GREEN_RANGE = ((20, 70, 0), (50, 255, 255))
+# GREEN_RANGE = ((8, 150, 110), (145, 255, 255))
+def ballTrackHough(image):
+    global prevCicle , dist
+    kernel = np.ones((5, 5), np.uint8)
+    colorLower, colorUpper = GREEN_RANGE
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    grayFrame = cv2.inRange(hsv, colorLower, colorUpper)
+    grayFrame = cv2.morphologyEx(grayFrame, cv2.MORPH_CLOSE, kernel)
+    # grayFrame = cv2.cvtColor(image_masked,cv2.COLOR_BGR2GRAY)
+    blurFrame = cv2.GaussianBlur(grayFrame,(9,9),2)
+    circles = cv2.HoughCircles(blurFrame , cv2.HOUGH_GRADIENT , 1 , 50,
+                              minRadius = 2 , maxRadius = 1000)
+    chosen = None
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        chosen = None
+        for x in circles[0]:
+            if chosen is None:chosen = x
+            if prevCicle is not None:
+                if dist(chosen[0],chosen[1],prevCicle[0],prevCicle[1]) >= dist(x[0],x[1],prevCicle[0],prevCicle[1]):
+                    chosen = x
+    return chosen , grayFrame
 cam = cv2.VideoCapture(0)
 last_3_frames = []
 while True:
@@ -10,18 +35,24 @@ while True:
     image = frame
     last_3_frames.append(frame)
     last_3_frames = last_3_frames[-3::]
+    green_image = frame
     #Ball Detection
     if len(last_3_frames) == 3:
+        #Deep ball Detection
         ball_track , dist = infer_model(last_3_frames,ball_model)
         ball_track = remove_outliers(ball_track, dist)
-        #Draw ball 
-        if ball_track[-1][0] is not None and ball_track[-1][1] is not None:
-            print("detected")  
-            x_ball_pred , y_ball_pred = ball_track[-1]
-            image = cv2.circle(image, (int(x_ball_pred), int(y_ball_pred)),
-                                    radius=0, color=(255,0, 0), thickness=10)
+        #Traditional Ball detection
+        ball , green_image = ballTrackHough(frame)
+        print("Ball : ",ball)
+        if ball is not None:
+            ball_track[-1] = ball[0:2]
+            #Draw ball 
+            if ball_track[-1][0] is not None and ball_track[-1][1] is not None:
+                x_ball_pred , y_ball_pred = ball_track[-1]
+                image = cv2.circle(image, (int(x_ball_pred), int(y_ball_pred)),
+                                        radius=0, color=(255,0, 0), thickness=10)
     # Display the captured frame
-    cv2.imshow('Camera', image)
+    cv2.imshow('Camera', green_image)
 
     # Press 'q' to exit the loop
     if cv2.waitKey(1) == ord('q'):
